d59dc070e3f5fbd41f4a8b83f3336eced702a4f1
Joseph23-11 <joseph.simarmata2311@mail.ugm.ac.id>
2024-02-28 22:20:42 +0700

    job listing page and components ButtonActionTable

---
+++ src/app/job-listings/page.tsx
@@ -0,0 +1,64 @@
+import { FC } from "react";
+import { Badge } from "@/components/ui/badge";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { JOB_LISTING_COLUMNS, JOB_LISTING_DATA } from "@/constant";
+import { Button } from "@/components/ui/button";
+import { MoreVertical } from "lucide-react";
+
+interface JobListingsPageProps {}
+
+export const revalidate = 0;
+
+const JobListingsPage: FC<JobListingsPageProps> = async ({}) => {
+  return (
+    <div>
+      <div className="font-semibold text-3xl">Job Listings</div>
+
+      <div className="mt-10">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {JOB_LISTING_COLUMNS.map((item: string, i: number) => (
+                <TableHead key={item + i}>{item}</TableHead>
+              ))}
+              <TableHead>Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {JOB_LISTING_DATA.map((item: any, i: number) => (
+              <TableRow key={item.roles + 1}>
+                <TableCell>{item.roles}</TableCell>
+                <TableCell>
+                  <Badge>{item.status}</Badge>
+                </TableCell>
+                <TableCell>{item.datePosted}</TableCell>
+                <TableCell>{item.dueDate}</TableCell>
+                <TableCell>
+                  <Badge variant="outline">{item.jobType}</Badge>
+                </TableCell>
+                <TableCell>{item.applicants}</TableCell>
+                <TableCell>
+                  {item.applicants} / {item.needs}
+                </TableCell>
+                <TableCell>
+                  <Button size="icon" variant="outline">
+                    <MoreVertical className="w-4 h-4" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default JobListingsPage;
